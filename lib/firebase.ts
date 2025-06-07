03641a790325e7e677b1b6942bc9029ee8d80ba9
Gasper Soos <gaspersoos12@gmail.com>
2025-06-07 18:26:40 +0200

    fix: remove unused analytics variable

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAfINjEAXqUzopBMyflwhcnTA_ieCfQLNw",
   authDomain: "atb-storitve.firebaseapp.com",
