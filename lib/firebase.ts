370d6371f727f0d34c7a2f63b3588e2d2c9b4b27
Gasper Soos <gaspersoos12@gmail.com>
2025-06-07 17:59:22 +0200

    initial commit

---
+++ lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfINjEAXqUzopBMyflwhcnTA_ieCfQLNw",
+  authDomain: "atb-storitve.firebaseapp.com",
+  projectId: "atb-storitve",
+  storageBucket: "atb-storitve.firebasestorage.app",
+  messagingSenderId: "521646612400",
+  appId: "1:521646612400:web:e65df0f5d68d80a2b50075",
+  measurementId: "G-BPKDW6FJYG"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
