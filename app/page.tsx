a3c4433b5240895d13b4ca13bdb6c554bf6594ca
Gasper Soos <gaspersoos12@gmail.com>
2025-06-08 15:17:34 +0200

    Test admin side + setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useState } from "react";
-import SearchRentals from "../components/admin/SearchRentals";
+import SearchRentals from "/components/admin/SearchRentals";
 
 export default function Home() {
   const [availableSuitcases, setAvailableSuitcases] = useState<string[] | null>(null);
 
   // Tu kasneje dodamo funkcijo, ki bo poizvedovala Firebase in nastavljala rezultate
-  const handleSearch = (startDate: string, endDate: string) => {
+  const handleSearch = (_startDate: string, _endDate: string) => {
     // Za test bo zaenkrat samo placeholder
     setAvailableSuitcases([
       "Kovček A",
